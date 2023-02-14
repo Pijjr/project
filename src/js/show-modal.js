f97a88557366f6e2267a9ea823dce53d1422793c
Raadius <raadius@yandex.ru>
2023-02-14 20:48:26 +0300

    Подкрутил поведение модальных окон при закрытии

--- src/js/show-modal.js
+++ src/js/show-modal.js
@@ -6,17 +6,22 @@ export let mainPage = document.querySelector('.main-content')
 export let burger = document.querySelector('.burger-menu')
 export let buttonChat = document.querySelector('.button-message-burger')
 export let buttonPhone = document.querySelector('.button-phone-burger')
+export let header = document.querySelector('header')
 
 buttonChat.addEventListener('click', function () {
   modalFeedback.classList.remove('modal-container--hidden')
   mainPage.classList.add('page-blur')
   burger.classList.add('page-blur')
+  header.classList.add('page-blur')
+  mainPage.classList.add('fixed')
 })
 
 buttonPhone.addEventListener('click', function () {
   modalCall.classList.remove('modal-container--hidden')
   mainPage.classList.add('page-blur')
   burger.classList.add('page-blur')
+  header.classList.add('page-blur')
+  mainPage.classList.add('fixed')
 })
 
 export let buttonCloseFeedback = document.querySelector(
@@ -28,22 +33,43 @@ buttonCloseFeedback.addEventListener('click', function () {
   modalFeedback.classList.add('modal-container--hidden')
   mainPage.classList.remove('page-blur')
   burger.classList.remove('page-blur')
+  header.classList.remove('page-blur')
+  mainPage.classList.remove('fixed')
 })
 
 buttonCloseChat.addEventListener('click', function () {
   modalCall.classList.add('modal-container--hidden')
   mainPage.classList.remove('page-blur')
   burger.classList.remove('page-blur')
+  header.classList.remove('page-blur')
+  mainPage.classList.remove('fixed')
 })
+
 export let headerButtonCall = document.querySelector('.button-phone')
 export let headerButtonChat = document.querySelector('.button-message')
 
 headerButtonCall.addEventListener('click', function () {
   modalFeedback.classList.remove('modal-container--hidden')
   mainPage.classList.add('page-blur')
+  header.classList.add('page-blur')
+  burger.classList.add('page-blur')
+  mainPage.classList.add('fixed')
 })
 
 headerButtonChat.addEventListener('click', function () {
   modalCall.classList.remove('modal-container--hidden')
   mainPage.classList.add('page-blur')
+  header.classList.add('page-blur')
+  burger.classList.add('page-blur')
+  mainPage.classList.add('fixed')
+})
+
+mainPage.addEventListener('click', function () {
+  if (window.innerWidth >= 1120) {
+    modalFeedback.classList.add('modal-container--hidden')
+    modalCall.classList.add('modal-container--hidden')
+    mainPage.classList.remove('page-blur')
+    burger.classList.remove('page-blur')
+    mainPage.classList.remove('fixed')
+  }
 })
